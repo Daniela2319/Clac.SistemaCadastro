293b9b35eab595185190de4a76149a9dc09b80ac
Daniela2319 <frankvt04@gmail.com>
2025-05-07 09:39:31 -0300

    Add project files.

---
+++ Clac.SistemaCadastro/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clac.SistemaCadastro
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá mundo!");
+            Console.WriteLine("What is your name:");
+
+            string name = Console.ReadLine();
+
+            Console.WriteLine($"My name is {name}");
+        }
+    }
+}
